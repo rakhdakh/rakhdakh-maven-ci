8ceec839233dccb2b8a110f0cbf542c5c2b6a35f
rakhdakh <ggigl11@freeuni.edu.ge>
2016-05-08 00:30:18 +0400

    Testing like a pro

---
+++ src/test/java/TestMyTATA.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Rakhdakh on 5/8/2016.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestMyTATA {
+
+    @Test
+    public void DoIThough(){
+        assertEquals(703,37*19);
+    }
+}
