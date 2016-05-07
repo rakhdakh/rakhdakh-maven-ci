d3e04e0b6ec733af6295ed00bd6f792bfa560318
rakhdakh <ggigl11@freeuni.edu.ge>
2016-05-08 01:25:18 +0400

    zaura da tatara dadzmobildnen, Mock Test gaketebulia

--- src/test/java/TestMyTATA.java
+++ src/test/java/TestMyTATA.java
@@ -3,12 +3,27 @@
  */
 
 import org.junit.Test;
+import org.mockito.*;
+
+import java.math.BigInteger;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class TestMyTATA {
+
+    @Mock
+    BigInteger bi;
 
     @Test
     public void DoIThough(){
         assertEquals(703,37*19);
+    }
+
+    @Test
+    public void AlwaysMockingBluff(){
+        BigInteger zaura = mock(BigInteger.class);
+        BigInteger tatara = zaura.not();
+        assertEquals(tatara,zaura.not());
     }
 }
